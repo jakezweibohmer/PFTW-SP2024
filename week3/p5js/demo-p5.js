4aaeafc82da17e89f01ade42cb1bbab0066e21ec
jakezweibohmer <2treesdesign@mchsi.com>
2024-01-28 14:53:12 -0600

    p5js demo

---
+++ week3/p5js/demo-p5.js
@@ -0,0 +1,28 @@
+let heading;
+let button;
+let input;
+let message = 'type something to change me';
+
+// using the DOM library for
+function sayMessage () {
+    message = input.value();
+    input.value('');
+}
+function setup (){
+    createCanvas(800, 600);
+    background('lightblue');
+    heading = createElement('h2', ['Howdy']);
+    heading.position(100,200);
+    button = createButton('alert message');
+    button.size(146, 30);
+    button.position(100, 275);
+    button.mousePressed(sayMessage);
+    input = createInput('');
+    input.position(100,250);
+}
+function draw () {
+    background('lightblue');
+    textSize(28);
+    fill('purple');
+    text(message, 100, 400);
+}
