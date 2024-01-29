89f5b24883ac059c5ae6f72698ea6c10ff17fb56
jakezweibohmer <2treesdesign@mchsi.com>
2024-01-29 16:33:38 -0600

    finished Spiro Demo

--- week3/spiro/spiro.js
+++ week3/spiro/spiro.js
@@ -11,16 +11,17 @@ function makeArm(rotateBy) {
   let alt = Math.round(rotateBy / 360);
   console.log(alt);
   noFill();
-  stroke(255);
+  stroke(0, 0, 255);
   strokeWeight(1);
-  ellipse(150, 150 + alt, 150 - alt);
+  ellipse(150, 150 + alt, 150 / alt);
+  // bezier(alt - 10, alt + 10, 10, 10, 400, 110, 90, 80);
 }
 
 function draw () {
   translate(300, 300);
   rotate(rotateBy);
   makeArm(rotateBy);
-  rotateBy += 5;
+  rotateBy += 5; // does the duplicate, repeat, rotate for spiro
 }
 
 function mousePressed() {
