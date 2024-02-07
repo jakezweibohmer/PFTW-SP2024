9f218d4bba80314bb211632a6d64f865a64f5009
jakezweibohmer <2treesdesign@mchsi.com>
2024-02-06 20:34:34 -0600

    revisons made based on class demo week 4

--- week4/ex4.A/memory.js
+++ week4/ex4.A/memory.js
@@ -8,10 +8,10 @@ let myCircles = [];
 let startingId = 0;
 
 function setup() {
-  createCanvas(1000, 500);
+  createCanvas(1000, 1000);
   background(0);
   // ellipse(circleX, circleY, circleDiameter);
-  for (let k = 0; k < 2; k++) {
+  for (let k = 0; k < 5; k++) {
     for (let i = 0; i < 4; i++) {
       ellipse(startingX, startingY, circleDiameter);
       myCircles.push({ x: startingX, y: startingY, id: startingId });
