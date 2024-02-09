d2ae1623c9e149cb9a30a43d6c8658f67c558aaf
jakezweibohmer <2treesdesign@mchsi.com>
2024-02-09 17:03:08 -0600

    finishing up outline for ex.4.A

--- week4/ex4.A/memory.js
+++ week4/ex4.A/memory.js
@@ -3,13 +3,13 @@ const circleDiameter = 100;
 // let circleX = 200;
 // let circleY = 300;
 let startingX = 200;
-let startingY = 100;
+let startingY = 200;
 let myCircles = [];
 let startingId = 0;
 
 function setup() {
-  createCanvas(1000, 1000);
-  background(0);
+  createCanvas(850, 1000);
+  background(60, 181, 195);
   // ellipse(circleX, circleY, circleDiameter);
   for (let k = 0; k < 5; k++) {
     for (let i = 0; i < 4; i++) {
@@ -29,7 +29,7 @@ function mousePressed() {
   for (let j = 0; j < myCircles.length; j++) {
     let distance = dist(mouseX, mouseY, myCircles[j].x, myCircles[j].y);
     if (distance < circleDiameter / 2) {
-      console.log("circle has been clicked", myCircles[j].id);
+      console.log("The circle has been clicked", myCircles[j].id);
     }
   }
 }
