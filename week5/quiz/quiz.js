f42258777a43077826482d4e52091a96ca36ae35
jakezweibohmer <2treesdesign@mchsi.com>
2024-02-11 14:01:18 -0600

    testing script for quiz

---
+++ week5/quiz/quiz.js
@@ -0,0 +1,17 @@
+/* const myPuppies = ["Lucky", "Lucy", "Limda", "Lacey", "Carl"];
+for (let i = 0; i < myPuppies.length; i++) {
+  console.log("My dog " + myPuppies[i] + " is a really good puppy.");
+} */
+
+let greekMythicalCharacters = [
+  "Athena",
+  "Zeus",
+  "Hermes",
+  "Hercules",
+  "Hera",
+  "that one guy",
+];
+greekMythicalCharacters.pop();
+greekMythicalCharacters.push("Hades");
+
+console.log(greekMythicalCharacters);
